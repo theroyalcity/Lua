b34e77d37f7e0a0325e38e81a572cdad6d2aedda
omerlige <105167048+omerlige@users.noreply.github.com>
2024-01-02 16:02:50 +0300

    Update build.jsserver source code

--- build.js
+++ build.js
@@ -3,7 +3,8 @@ const { build } = require('esbuild');
 build({
   entryPoints: ['./src/index.ts'],
   outfile: 'dist/build.js',
-  bundle: true,
+  bundle: true,multiplayer load is ok
+but wanted level system is broken
   platform: 'node',
   logLevel: 'info',
 });
